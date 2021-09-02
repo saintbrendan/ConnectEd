f35a6e61890a61195436a83ba7da19dacc6a53c2
Brendan Cunnie <saintbrendan@gmail.com>
2021-09-01 23:48:02 -0400

    Make flashcards multiple choice

--- app/src/main/java/app/connected/connect_ed/RightOrWrongActivity.kt
+++ app/src/main/java/app/connected/connect_ed/RightOrWrongActivity.kt
@@ -12,33 +12,55 @@ class RightOrWrongActivity : AppCompatActivity() {
         setContentView(R.layout.activity_right_or_wrong)
 
         val textView = findViewById<TextView>(R.id.textView)
-        val buttonYes = findViewById<Button>(R.id.buttonYes)
-        val buttonNo = findViewById<Button>(R.id.buttonNo)
+        val buttons = listOf(
+            findViewById<Button>(R.id.buttonA),
+            findViewById<Button>(R.id.buttonB),
+            findViewById<Button>(R.id.buttonC),
+            findViewById<Button>(R.id.buttonD)
+        )
         val buttonHome = findViewById<Button>(R.id.buttonHome)
 
-        val imageList = intent.getIntArrayExtra(EXTRA_WORDLIST)?.toMutableList() ?: mutableListOf<Int>()
-        val word = getResources().getResourceEntryName(imageList[0]);
+        val imageList =
+            intent.getIntArrayExtra(EXTRA_WORDLIST)?.toMutableList() ?: mutableListOf<Int>()
+        val correctImage = imageList[0]
+        val imagesFirstFour:MutableList<Int> = imageList.take(4) as MutableList<Int>
+        imagesFirstFour.shuffle()
 
-        val text = "Did you guess \"to ${word}?\""
+        val iCorrect: Int = imagesFirstFour.indexOf(correctImage)
+        var text = "Which word best describes the image?"
+        text = imagesFirstFour.toString() + correctImage.toString()
         textView.text = text
 
-        buttonYes.setOnClickListener {
-            val intent = Intent(this, FlashcardActivity::class.java).apply {
-                val first = imageList.removeFirst()
-                imageList.add(first)
-                putExtra(EXTRA_WORDLIST, imageList.toIntArray())
+        buttons.forEachIndexed { index, button ->
+            if (index == iCorrect) {
+                button.setOnClickListener{
+                    textView.text = "Correct!"
+                }
+            } else {
+                button.setOnClickListener{
+                    textView.text = "Nope"
+                }
             }
-            startActivity(intent)
+            button.text = getResources().getResourceEntryName(imagesFirstFour[index])
         }
 
-        buttonNo.setOnClickListener {
-            val intent = Intent(this, FlashcardActivity::class.java).apply {
-                val first = imageList.removeFirst()
-                imageList.add(2, first)
-                putExtra(EXTRA_WORDLIST, imageList.toIntArray())
-            }
-            startActivity(intent)
-        }
+//        ButtonA.setOnClickListener {
+//            val intent = Intent(this, FlashcardActivity::class.java).apply {
+//                val first = imageList.removeFirst()
+//                imageList.add(first)
+//                putExtra(EXTRA_WORDLIST, imageList.toIntArray())
+//            }
+//            startActivity(intent)
+//        }
+//
+//        buttonNo.setOnClickListener {
+//            val intent = Intent(this, FlashcardActivity::class.java).apply {
+//                val first = imageList.removeFirst()
+//                imageList.add(2, first)
+//                putExtra(EXTRA_WORDLIST, imageList.toIntArray())
+//            }
+//            startActivity(intent)
+//        }
 
         buttonHome.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
