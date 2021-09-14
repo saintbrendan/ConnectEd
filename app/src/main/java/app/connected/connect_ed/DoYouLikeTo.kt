a1e9d2ff659809fa4924afa99baa8ab9a4daa53a
Erik Tarango <tarango.e.a@gmail.com>
2021-09-13 17:02:16 -0700

    Added Do you like to text change

--- app/src/main/java/app/connected/connect_ed/DoYouLikeTo.kt
+++ app/src/main/java/app/connected/connect_ed/DoYouLikeTo.kt
@@ -2,10 +2,34 @@ package app.connected.connect_ed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+
 
 class DoYouLikeTo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_do_you_like_to)
+        val correctAnswer = "walk"
+
+        val inputText = findViewById<EditText>(R.id.editTextTextPersonName);
+
+        inputText.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                val t = inputText.text.toString()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                val t = inputText.text.toString()
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val t = inputText.text.toString()
+                if (inputText.text.toString() == correctAnswer) {
+                    inputText.setTextColor(0xFF0000)
+                }
+            }
+        })
     }
 }
