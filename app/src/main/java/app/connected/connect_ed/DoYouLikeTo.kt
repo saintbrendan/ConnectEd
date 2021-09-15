515add3d5655b70c51bc174c0b50e3ad3e1977d0
Erik Tarango <tarango.e.a@gmail.com>
2021-09-15 16:49:39 -0700

    Added confirm button to DoYouLikeTo

--- app/src/main/java/app/connected/connect_ed/DoYouLikeTo.kt
+++ app/src/main/java/app/connected/connect_ed/DoYouLikeTo.kt
@@ -1,19 +1,41 @@
 package app.connected.connect_ed
 
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 
 
 class DoYouLikeTo : AppCompatActivity() {
+    var imageList = mutableListOf<Int>(
+        R.drawable.rest,
+        R.drawable.climb,
+        R.drawable.argue,
+        R.drawable.cry,
+        R.drawable.dance,
+        R.drawable.dive,
+        R.drawable.exercise,
+        R.drawable.windsurf,
+        R.drawable.walk,
+        R.drawable.write,
+        R.drawable.cook,
+    )
+    var imgIndex = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_do_you_like_to)
         val correctAnswer = "walk"
 
         val inputText = findViewById<EditText>(R.id.editTextTextPersonName);
+        val imgView = findViewById<ImageView>(R.id.imageView2)
+        val buttonCorrect = findViewById<Button>(R.id.buttonCorrect)
 
         inputText.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
@@ -27,9 +49,19 @@ class DoYouLikeTo : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 val t = inputText.text.toString()
                 if (inputText.text.toString() == correctAnswer) {
-                    inputText.setTextColor(0xFF0000)
+                    inputText.setTextColor(Color.GREEN)
+                    buttonCorrect.visibility = View.VISIBLE
                 }
             }
         })
+
+        buttonCorrect.setOnClickListener {
+            buttonCorrect.visibility = View.INVISIBLE
+            inputText.text.clear()
+            val d = ResourcesCompat.getDrawable(resources, R.drawable.climb, null)
+            imgView.setImageDrawable(d)
+        }
+
+
     }
 }
