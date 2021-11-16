d51ea257947d4bf65bf7fae920103d1fb38c1c67
Brendan Cunnie <saintbrendan@gmail.com>
2021-11-15 19:41:54 -0500

    Implement initial voice recognition

--- app/src/main/java/app/connected/connect_ed/FlashcardActivity.kt
+++ app/src/main/java/app/connected/connect_ed/FlashcardActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.RecognizerIntent
 import android.util.DisplayMetrics
-import android.util.Log
+import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import java.util.*
 
 const val EXTRA_WORDLIST = "com.example.mergeded.WORDLIST"
 const val EXTRA_SOUNDLIST = "com.example.mergeded.SOUNDLIST"
@@ -26,6 +30,7 @@ class FlashcardActivity : AppCompatActivity() {
         R.raw.write,
         R.raw.cook,
     )
+    var verb:String = ""
 
     private var mediaPlayer: MediaPlayer? = null
 
@@ -34,6 +39,8 @@ class FlashcardActivity : AppCompatActivity() {
         setContentView(R.layout.activity_flashcard)
 
         val imageUserListens = findViewById<ImageView>(R.id.imageUserListens);
+        val imageUserSpeaks = findViewById<ImageView>(R.id.imageUserSpeaks);
+        val textRight = findViewById<TextView>(R.id.textRight);
 
         fun getDrawablesList(): List<Int> {
             val drawableClass = R.drawable::class
@@ -66,11 +73,45 @@ class FlashcardActivity : AppCompatActivity() {
         }
         mediaPlayer?.start()
 
+        verb = getResources().getResourceEntryName(newList[0])
+        verb = verb.removePrefix("image_").replace("_", " ")
+
+
         //imageView.setImageResource(imageList[0])
         val displayMetrics: DisplayMetrics = getResources().getDisplayMetrics()
         Glide.with(this).load(
             newList[0]
         ).override(displayMetrics.widthPixels, displayMetrics.heightPixels).into(imageView)
+
+        fun getSpeechInput() {
+            val intent = Intent(
+                RecognizerIntent
+                    .ACTION_RECOGNIZE_SPEECH
+            )
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+            )
+            intent.putExtra(
+                RecognizerIntent.EXTRA_LANGUAGE,
+                Locale.getDefault()
+            )
+
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, 10)
+            } else {
+                Toast.makeText(
+                    this,
+                    "Your Device Doesn't Support Speech Input",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+
+        imageUserSpeaks.setOnClickListener(View.OnClickListener {
+            getSpeechInput()
+        })
 
         imageUserListens.setOnClickListener {
             mediaPlayer?.start()
@@ -82,6 +123,43 @@ class FlashcardActivity : AppCompatActivity() {
                 putExtra(EXTRA_SOUNDLIST, soundList.toIntArray())
             }
             startActivity(intent)
+        }
+        textRight.setOnClickListener {
+            val intent = Intent(this, RightOrWrongActivity::class.java).apply {
+                putExtra(EXTRA_WORDLIST, newList.toIntArray())
+                putExtra(EXTRA_SOUNDLIST, soundList.toIntArray())
+            }
+            startActivity(intent)
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(
+            requestCode,
+            resultCode, data
+        )
+        val textResult = findViewById<TextView>(R.id.textResult);
+
+        when (requestCode) {
+            10 -> if (resultCode == RESULT_OK &&
+                data != null
+            ) {
+                val result =
+                    data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS
+                    )
+                val spokenText = result?.get(0)?.lowercase()
+                var msg = ""
+                if (spokenText == verb.lowercase() || spokenText == "to "+verb.lowercase()) {
+                    msg = "Congratulations!  Correct!  \nClick the right arrow to continue."
+                } else {
+                    msg = "you said " + spokenText + " and we were expecting " + verb
+                }
+                textResult.text = msg
+            }
         }
     }
 
